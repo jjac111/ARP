d6a2177be35ca0a69501c8d18654ca61ab4cfd82
Juan J Arosemena <40127035+jjac111@users.noreply.github.com>
2020-03-11 14:04:34 -0500

    ARP works properly, needs formatting for requirements
    * dr_arp_discovery: Writes .txt files for the results, but they are not formatted to the requirements.
    * Report: Nothing done yet really.

--- dr_arp_discovery.py
+++ dr_arp_discovery.py
@@ -1,39 +1,66 @@
 #This code is based on https://null-byte.wonderhowto.com/how-to/build-arp-scanner-using-scapy-and-python-0162731/
 import sys
-from datetime import datetime
+import os
+import platform
+from datetime import datetime, date
 from scapy.all import srp, Ether, ARP, conf
 from mac_vendor_lookup import MacLookup
 
+
 if __name__ == "__main__":
 
-    try:
-        interface = input("Enter desired interface: ")
-        ips = input("Enter range of IPs to Scan for: ")
+	try:
+		interface = input("Enter desired interface: ")
+		ips = input("Enter range of IPs to Scan for: ")
 
-    except KeyboardInterrupt:
-        print ("\n User requested shutdown")
-        print ("Quiting...")
-        sys.exit(1)
+	except KeyboardInterrupt:
+		print ("\n User requested shutdown")
+		print ("Quiting...")
+		sys.exit(1)
 
-    print ("\n Scanning... ")
-    start_time = datetime.now()
+	print ("\n Scanning... ")
+	start_time = datetime.now()
 
-    conf.verb = 0
-    ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ips), timeout=15, iface=interface, verbose=False)
-
-    #print ans.summary()
-    print ('MAC - IP')
-    for s, r in ans:
-        print (r.sprintf(r'%Ether.hwsrc% - %ARP.psrc%'))
-
-    stop_time = datetime.now()
-    total_time = stop_time - start_time
+	conf.verb = 0
 	
-    print ("\n Scan Complete!")
-    print ("Scan Duration: %s" %(total_time))
-    print()
+	ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ips), timeout=15, iface=interface, verbose=False)
+	
+	#print ans.summary()
+	print ('MAC - IP')
+	for s, r in ans:
+		print (r.sprintf(r'%Ether.hwsrc% - %ARP.psrc%'))
 
-    ML = MacLookup()
-    for a in ans:
-        mac = a[1].hwsrc
-        print(ML.lookup(mac))
+	stop_time = datetime.now()
+	total_time = stop_time - start_time
+	
+	print ("\n Scan Complete!")
+	print ("Scan Duration: %s" %(total_time))
+	print()
+
+	ML = MacLookup()
+	
+	to_write = []
+	
+	for a in ans:
+		mac = a[1].hwsrc
+		ip = a[1].psrc
+		try:
+			lkup = ML.lookup(str(mac))
+			to_write.append((ip, mac, lkup))
+			print(lkup)
+		except Exception as e:
+			print(e)
+			to_write.append((ip, mac, "U N K N O W N"))
+			
+	filename = "AROSEMENA_AYALA_" + ('WLAN' if interface == 'Wi-Fi' else 'LAN') + '_' + str(stop_time.year) + '.' + str(stop_time.month) + '.' + str(stop_time.day) + '.' + str(stop_time.hour) + '.' + str(stop_time.minute) + '.' + str(stop_time.second) + '.txt'
+	directory = 'results'
+	
+	if not os.path.exists(directory):
+		os.makedirs(directory)
+	
+	with open(directory + '/' + filename, 'w') as f:
+		string = ""
+		for ip, mac, lkup in to_write:
+			string += f"{ip}\t{mac}\t{lkup}\n"
+			
+		f.write(string)
